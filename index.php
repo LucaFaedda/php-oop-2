d69dec98005cc43c978a4f509acb7e9e3398ef5c
LucaFaedda <lucakarl91@gmail.com>
2023-02-06 21:36:29 +0100

    aggiunta cartella style

--- index.php
+++ index.php
@@ -14,25 +14,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/style.css">
     <title>OOP-2</title>
 </head>
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-12 d-flex flex-wrap">
+        <div class="row my-5">
+            <div class="col-12 text-center my-4">
+                <h1 class="text-danger">Boolshop</h1>
+            </div>
+            <div class="col-12 d-flex flex-wrap justify-content-center ">
                 <?php foreach($prodotti as $item){ ?>
-                    <div class="card m-5 p-2 d-flex justify-content-around" style="width: 18rem">
-                        <div class="d-flex justify-content-center">
-                            <img src="<?php echo $item->immagine;
-                            ?>" class=" w-50" alt="...">
-
+                    <div class="card d-flex justify-content-around m-2 " style="width: 20rem;">
+                        <div class="my-2 mx-1 pb-3 border-bottom">
+                            <img src="<?php echo $item->immagine;?>" class="card-img-top immagine" alt="<?php $item->name ?>">
                         </div>
-                        <div >
-                            <?php echo $item->getProduct(); // metodo per poter stampare le informazioni. 
-                            ?>
+                        <div class="card-body">
+                            <?php echo $item->getProduct();?>
                         </div>
                     </div>
-               <?php }?>
+                <?php }?>
             </div>
         </div>
     </div>
